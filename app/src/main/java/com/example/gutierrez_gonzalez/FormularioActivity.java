4a79ef688af883d234b046075a5f96790b79ffbf
FateDGG <anghelandres12@gmail.com>
2023-04-12 14:26:15 -0500

    12/04/2023 - FormularioActivity 2.0

--- app/src/main/java/com/example/gutierrez_gonzalez/FormularioActivity.java
+++ app/src/main/java/com/example/gutierrez_gonzalez/FormularioActivity.java
@@ -9,16 +9,16 @@ import android.widget.TextView;
 
 public class FormularioActivity extends AppCompatActivity {
 
-    ImageView ivRegistro;
-    TextView tvNombreReg, tvPrecioReg;
+
+    TextView tvNombreReg, tvPrecioReg, tvImgReg;
     Button btnRegistrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
-        ivRegistro = findViewById(R.id.iv_registro);
         tvNombreReg = findViewById(R.id.tv_nombreReg);
         tvPrecioReg = findViewById(R.id.tv_precioReg);
         btnRegistrar = findViewById(R.id.btn_Registrar);
+        tvImgReg = findViewById(R.id.tv_imgRegistro);
     }
 }
